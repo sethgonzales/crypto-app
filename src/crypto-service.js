4838fb8d073a2e122765698415e290d2feaf23da
sethgonzales <sethgonzales157@gmail.com>
2023-09-07 14:15:28 -0700

    add response for country values

--- src/crypto-service.js
+++ src/crypto-service.js
@@ -1,15 +1,15 @@
 export default class CryptoService {  
-  static async getCryptoData(cryptocurrency) { //call this method, make it async 
-    try { //wrap our code in a try...catch block to handle errors as async will not be able to resolve or reject errors.
-      const response = await fetch(`https://api.coingecko.com/api/v3/coins/${cryptocurrency}?localization=true&tickers=true&market_data=true`); //fetch the api and gather response
-      const jsonifiedResponse = await response.json(); //parse the response into json lang. await to make sure each thing is done in order.
-      if (!response.ok) { //if the response is not okay...
+  static async getCryptoData(cryptocurrency) { 
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/coins/${cryptocurrency}?localization=true&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`);
+      const jsonifiedResponse = await response.json(); 
+      if (!response.ok) { 
         const errorMessage = `${response.status} ${response.statusText}
-        ${jsonifiedResponse.message}`; //create an error message from the text from the jsonified api data 
-        throw new Error(errorMessage); //throw to an error function
+        ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
       }
-      return jsonifiedResponse;//if everything is OK, provide the returned response from the api data
-    } catch(error) { //catch error and return error message
+      return jsonifiedResponse;
+    } catch(error) {
       return error;
     }
   }
--- src/index.js
+++ src/index.js
@@ -8,30 +8,53 @@ import CryptoService from './crypto-service.js';
 
 async function getCryptoData(cryptocurrency) {
   const response = await CryptoService.getCryptoData(cryptocurrency);
-  if (response.main) {
+  if (response) {
     printCrypto(response);
   } else {
     printError(response);
   }
 }
 
+
+// UI Logic
 function printCrypto(response) {
-  document.querySelector('#showResponse').innerHTML = 
-  <h2>The something in ${response} is</h2>;
+  document.querySelector('#showName').innerHTML =
+    `<h2><img src=${response.image.small}> ${response.name} </h2>`;
+
+
+    document.querySelector('#showPrice').innerHTML =
+    `<h2><img src=${response.image.small}> ${response.name} </h2>`;
+
+  let date = new Date(response.market_data.ath_date.usd).toDateString(); //auth date to date formatting
+  document.querySelector('#showAuth').innerHTML =
+    `<p>Last Authorized: ${date}</p>`
+
+
+  
+  //show currency value in all countries
+    const currPrice = Object.keys(response.market_data.current_price); //turn object into an array holding key-value pairs
+  for (let i = 0; i < currPrice.length; i++) { //for each item in the array...
+    let priceKey = currPrice[i]; //The Key at each index
+    const price = response.market_data.current_price[priceValue].toLocaleString('en-US'); //the value at each key written in us number notation
+    document.querySelector('#showCountryPrice').innerHTML +=
+      `<p>${priceKey}: $${price}</p>`; //show the key, value pair
+  }
 }
 
-function printError(error) {
-  document.querySelector('#showResponse').innerHTML = 
-  <h2>There was an error accessing the data for ${error}.`</h2>;
-}
 
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const cryptocurrency = document.querySelector('#showResponse').value;
-  document.querySelector('#showResponse').value = null;
-  getCryptoData(cryptocurrency);
-}
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
-});
+  function printError(error) {
+    document.querySelector('#showResponse').innerHTML =
+      `<h2>There was an error accessing the data for ${error}.</h2>`;
+  }
+
+  function handleFormSubmission(event) {
+    event.preventDefault();
+    const cryptocurrency = document.querySelector('#crypto').value;
+    document.querySelector('#crypto').value = null;
+    getCryptoData(cryptocurrency);
+  }
+
+  window.addEventListener("load", function () {
+    document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  });
